62b53fbdc73ea960d57e0bd04db442318565676e
diogopcaires <diogo.pires@nubank.com.br>
2021-10-18 14:09:23 -0300

    module 2

--- src/main/java/br/com/alura/studykafka/NewOrderMain.java
+++ src/main/java/br/com/alura/studykafka/NewOrderMain.java
@@ -4,22 +4,18 @@ import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
-import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer = new KafkaProducer<String, String>(properties());
-
-        var key = UUID.randomUUID();
-
-        //new order message
         var value = "#12331,123,520";
-        var record = new ProducerRecord("STORE_NEW_ORDER", key.toString(), value);
 
-        //email message
+        var record = new ProducerRecord("STORE_NEW_ORDER", value, value);
+
         var email = "Thanks for your order! We are processing your order!";
-        var emailRecord = new ProducerRecord("STORE_SEND_EMAIL", key.toString(), email);
+
+        var emailRecord = new ProducerRecord("STORE_SEND_EMAIL", email, email);
 
         producer.send(record, getCallback()).get();
         producer.send(emailRecord, getCallback()).get();
--- src/main/java/br/com/alura/studykafka/consumers/FraudDetectorService.java
+++ src/main/java/br/com/alura/studykafka/consumers/FraudDetectorService.java
@@ -7,27 +7,10 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 public class FraudDetectorService {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            var consumerid = i + "";
-            new Thread(() -> {
-                var consumer = new AntiFraudConsumer(consumerid);
-                consumer.start();
-            }).start();
-        }
-    }
-}
-
-class AntiFraudConsumer {
-    private String _id;
-
-    public AntiFraudConsumer(String id) {
-        this._id = id;
-    }
-
-    public void start() {
         var consumer = new KafkaConsumer<String, String>(properties());
         consumer.subscribe(Collections.singletonList("STORE_NEW_ORDER"));
 
@@ -37,7 +20,6 @@ class AntiFraudConsumer {
 
                 for (var record : records) {
                     System.out.println("-----------------------");
-                    System.out.println("Consumer " + _id + " processing Messages");
                     System.out.println(record.key());
                     System.out.println(record.value());
                     System.out.println(record.partition());
@@ -54,12 +36,12 @@ class AntiFraudConsumer {
         }
     }
 
-    private Properties properties() {
+    private static Properties properties() {
         var properties = new Properties();
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "batatinhafrita123");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
         return properties;
     }
 }
